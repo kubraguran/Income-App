d9c2f2eab4915dcccdea3ce573693f06e88efce7
kubraguran <81765398+kubraguran@users.noreply.github.com>
2021-11-21 13:29:34 +0300

    Add files via upload

---
+++ Income -app/invoice.js
@@ -0,0 +1,14 @@
+/*
+function start () {
+    let submit = document.getElementById("submit");
+    submit.addEventListener("click", toggle);
+};
+
+function toggle () {
+    let color = document.getElementById(body);
+    let backColor = color.style.backgroundColor;
+    color.style.backgroundColor = backColor === "background-image" ? "white" : "background-image";
+};
+
+start();
+*/
